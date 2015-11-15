54ca54440f72da955e9f4397679873e79ccf2cd3
Dragonite <weiqiangdragonite@gmail.com>
2015-11-15 19:07:48 +0800

    state that i am still living!
    Signed-off-by: Dragonite <weiqiangdragonite@gmail.com>

---
+++ unix_linux/linux_program/uup/ch3/ls1.c
@@ -0,0 +1,50 @@
+/*
+ * ls1.c
+ * purpose list contents of directory or directories
+ * action if no args, use . list files in args
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+void do_ls(char *dirname);
+
+
+int
+main(int argc, char *argv[])
+{
+	if (argc == 1)
+		do_ls(".");
+	else {
+		while (--argc) {
+			printf("%s:\n", *++argv);
+			do_ls(*argv);
+		}
+	}
+
+	return 0;
+}
+
+/*
+ * loist files in directory called dirname
+ */
+void
+do_ls(char *dirname)
+{
+	DIR *dir_ptr;
+	struct dirent *direntp;
+
+	if ((dir_ptr = opendir(dirname)) == NULL) {
+		fprintf(stderr, "ls1: cannot open %s\n", dirname);
+	} else {
+		while ((direntp = readdir(dir_ptr)) != NULL)
+			printf("%s\n", direntp->d_name);
+		closedir(dir_ptr);
+	}
+}
+
+
+
+
