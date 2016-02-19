d8230386e6d0a212afcc4af351f520eb79849777
Dragonite <weiqiangdragonite@gmail.com>
2016-02-19 23:06:20 +0800

    Signed-off-by: Dragonite <weiqiangdragonite@gmail.com>

--- unix_linux/Linux_network/unp/mycode/ch13_daemon/daemon_init.c
+++ unix_linux/Linux_network/unp/mycode/ch13_daemon/daemon_init.c
@@ -1,5 +1,5 @@
 /*
- *
+ * 以TLPI为主
  */
 
 #include <stdlib.h>
@@ -18,16 +18,22 @@ daemon_init(const char *pname, int facility)
 	int i;
 	pid_t pid;
 
+
+	/* Step 1 - fork() */
 	if ((pid = fork()) < 0)
 		return -1;
 	else if (pid != 0)
 		_exit(EXIT_SUCCESS);	/* parent terminates */
 
+
+	/* Step 2 - setid() */
 	/* child 1 continues */
 	/* become session leader */
 	if (setsid() == -1)
 		return -1;
 
+
+	/* Step 3 - Ignore SIGHUP and fork() */
 	signal(SIGHUP, SIG_IGN);
 
 	if ((pid = fork()) < 0)
@@ -35,14 +41,20 @@ daemon_init(const char *pname, int facility)
 	else if (pid != 0)
 		_exit(EXIT_SUCCESS);	/* child 1 terminates */
 
+
+	/* Step 4 */
 	/* child 2 continues */
 	/* change to root directory */
 	chdir("/");
 
+
+	/* Step 5 */
 	/* close all open files */
 	for (i = 0; i < MAXFD; ++i)
 		close(i);
 
+
+	/* Step 6 */
 	/* redirect stdin, stdout, and stderr to /dev/null */
 	open("/dev/null", O_RDONLY);
 	open("/dev/null", O_RDWR);
