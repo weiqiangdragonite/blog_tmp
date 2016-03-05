07baace367b3e25d3fc1d93be5b12632489e3e5f
Dragonite <weiqiangdragonite@gmail.com>
2016-03-05 18:04:16 +0800

    update sdl learning

--- SDL/SDL2_code/lesson_7/texture_render.c
+++ SDL/SDL2_code/lesson_7/texture_render.c
@@ -1,13 +1,13 @@
 /*
- * take a smaller image and stretch it to fit the screen.
+ *  loading an image to render it using textures
  *
  * gcc xxx -lSDL2 -l SDL2_image
  */
 
+
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-
 
 
 /* screen dimension constants */
@@ -17,11 +17,11 @@ const int SCREEN_HEIGHT = 480;
 /* the window we will be rendering to */
 SDL_Window *window = NULL;
 
-/* the surface contained by the window */
-SDL_Surface *screen_surface = NULL;
+/* window renderer */
+SDL_Renderer *renderer = NULL;
 
-/* current display image surface */
-SDL_Surface *current_surface = NULL;
+/* current display texture */
+SDL_Texture *current_texture = NULL;
 
 
 /* starts up SDL and create window */
@@ -33,8 +33,9 @@ int load_media(void);
 /* free media and shutdown SDL */
 void close_sdl(void);
 
-/* loads individual image */
-SDL_Surface *load_surface(const char *path);
+/* loads individual image as texture */
+SDL_Texture *load_texture(const char *path);
+
 
 
 
@@ -70,12 +71,14 @@ main(int argc, char *argv[])
 				break;
 			}
 
-			/* apply the PNG image */
-			SDL_BlitSurface(current_surface, NULL,
-					screen_surface, NULL);
+			/* clear screen */
+			SDL_RenderClear(renderer);
 
-			/* update the surface */
-			SDL_UpdateWindowSurface(window);
+			/* render texture to screen */
+			SDL_RenderCopy(renderer, current_texture, NULL, NULL);
+
+			/* update screen */
+			SDL_RenderPresent(renderer);
 		}
 	}
 
@@ -84,6 +87,10 @@ main(int argc, char *argv[])
 
 	return 0;
 }
+
+
+
+
 
 
 
@@ -98,6 +105,11 @@ init_sdl(void)
 				"SDL Error: %s\n", SDL_GetError());
 		return -1;
 	}
+
+	/* set texture filtering to linear */
+	if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+		fprintf(stderr,
+			"Warning: Linear texture filtering not enabled!\n");
 
 
 	/* create window */
@@ -114,6 +126,17 @@ init_sdl(void)
 		return -1;
 	}
 
+	/* create renderer for window */
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == NULL) {
+		fprintf(stderr, "Renderer could not be created! "
+			"SDL Error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	/* initialize renderer color */
+	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
 
 	/* initialize PNG loading */
 	flags = IMG_INIT_PNG;
@@ -121,13 +144,11 @@ init_sdl(void)
 		fprintf(stderr, "SDL_image could not initialize! "
 			"SDL_image Error: %s\n", IMG_GetError());
 		return -1;
-	} else {
-		/* get window surface */
-		screen_surface = SDL_GetWindowSurface(window);
 	}
 
 	return 0;
 }
+
 
 
 int
@@ -135,9 +156,9 @@ load_media(void)
 {
 	int success = 0;
 
-	current_surface = load_surface("loaded.png");
-	if (current_surface == NULL) {
-		fprintf(stderr, "Failed to load PNG image!\n");
+	current_texture = load_texture("texture.png");
+	if (current_texture == NULL) {
+		fprintf(stderr, "Failed to load texture image!\n");
 		success = -1;
 	}
 
@@ -149,13 +170,14 @@ void
 close_sdl(void)
 {
 	/* free load image */
-	SDL_FreeSurface(current_surface);
-	current_surface = NULL;
+	SDL_DestroyTexture(current_texture);
+	current_texture = NULL;
 
 	/* destroy window */
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	window = NULL;
-	
+	renderer = NULL;
 
 	/* quit SDL subsystems */
 	IMG_Quit();
@@ -163,33 +185,34 @@ close_sdl(void)
 }
 
 
-SDL_Surface *
-load_surface(const char *path)
+SDL_Texture *
+load_texture(const char *path)
 {
-	/* the final otimized image */
-	SDL_Surface *optimized_surface = NULL;
-
+	/* the final texture */
+	SDL_Texture *texture = NULL;
 
 	/* load image at specified path */
-	SDL_Surface *img_surface = IMG_Load(path);
+	SDL_Surface *load_surface = IMG_Load(path);
 
-	if (img_surface == NULL)
+
+	if (load_surface == NULL)
 		fprintf(stderr, "Unable to load image: %s!"
 			"SDL_image Error: %s\n", path, IMG_GetError());
 	else {
-		/* convert surface to screen format */
-		optimized_surface = SDL_ConvertSurface(
-			img_surface, screen_surface->format, 0);
+		/* create texture from surface pixels */
+		texture = SDL_CreateTextureFromSurface(renderer, load_surface);
 
-		if (optimized_surface == NULL)
-			fprintf(stderr, "Unable to otimized image: %s! "
+		if (texture == NULL)
+			fprintf(stderr, "Unable to create texture form %s! "
 				"SDL Error: %s\n", path, SDL_GetError());
 
 		/* free old loaded surface */
-		SDL_FreeSurface(img_surface);
+		SDL_FreeSurface(load_surface);
 	}
 
-	return optimized_surface;
+	return texture;
 }
+
+
 
 
