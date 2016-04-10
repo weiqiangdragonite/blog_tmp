bef0a10ae8624efd91434827ecf00ff141abcb27
Dragonite <weiqiangdragonite@gmail.com>
2016-04-10 23:51:43 +0800

    add dhcp, not finished

---
+++ unix_linux/Linux_network/dhcp/dhcp_client.c
@@ -0,0 +1,37 @@
+/*
+ *
+ */
+
+
+
+
+int
+main()
+{
+
+}
+
+
+dhcp_client()
+{
+	/** FIRST TIME */
+
+	/* send DHCP Discover packages to DHCP Server */
+
+
+	/* wait for DHCP Server send back DHCP Offer package */
+
+
+	/* check for empty IP, if OK, send DHCP Request for accept */
+	/* otherwise send DHCP Decline to refused and restart again */
+
+
+	/* receive DHCP ACK, check the IP for ensure, then start */
+
+
+
+}
+
+
+
+
---
+++ unix_linux/Linux_network/dhcp/dhcp_server.c
@@ -0,0 +1,34 @@
+/*
+ *
+ */
+
+
+
+
+
+
+
+int
+main()
+{
+
+}
+
+
+dhcp_server()
+{
+	/* listen, wait for client DHCP Discover packages */
+
+
+	/* check for empty IP, send client DHCP Offer packages */
+
+
+	/* check for client DHCP Request, if is accepted, send back DHCP ACK */
+	/* otherwise cancled */
+
+
+
+}
+
+
+
