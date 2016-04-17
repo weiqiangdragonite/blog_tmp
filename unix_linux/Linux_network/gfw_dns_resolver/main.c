2824e60a831c163d166da4b80455caae67996965
Dragonite <weiqiangdragonite@gmail.com>
2016-04-17 23:34:05 +0800

    update dns

--- unix_linux/Linux_network/gfw_dns_resolver/main.c
+++ unix_linux/Linux_network/gfw_dns_resolver/main.c
@@ -76,10 +76,10 @@ struct dnshdr {
 #endif
 		};
 	};
-	unsigned short questions;
-	unsigned short answers;
-	unsigned short authority;
-	unsigned short additional;
+	unsigned short questions;	/* 问题数 */
+	unsigned short answers;		/* 回答RR数 */
+	unsigned short authority;	/* 权威RR数 */
+	unsigned short additional;	/* 附加RR数 */
 };
 
 
@@ -88,7 +88,9 @@ struct dnshdr {
 void gfw_resolve(const char *hostname, char *out_ip);
 void get_hostname(const char *hostname, char *out_ip);
 int is_bad_ip(char *ip);
-char *build_request_data(const char *hostname, int *size);
+char *build_request_data(char *hostname, int *size);
+void decode_dns_response(char *buf, const char *hostname, char *ip);
+
 
 
 
@@ -129,6 +131,7 @@ gfw_resolve(const char *hostname, char *out_ip)
 	int data_size;
 	int i, n;
 	char recv_buf[BUFSIZE];
+	char *host_buf = NULL;
 
 	/* 根据hostname找出ip */
 	//get_hostname(hostname, out_ip);
@@ -152,8 +155,7 @@ gfw_resolve(const char *hostname, char *out_ip)
 	memset(&dest, 0, sizeof(dest));
 	dest.sin_family = AF_INET;
 	dest.sin_port = htons(53);
-	//dest.sin_addr.s_addr = inet_addr(DNS_SERVER);
-
+	//dest.sin_addr.s_addr = inet_addr(DNS_SERVER);	// 不推荐inet_addr()
 	if (inet_pton(AF_INET, DNS_SERVER, &(dest.sin_addr)) != 1) {
 		fprintf(stderr, "inet_pton() convert %s failed: %s\n",
 			DNS_SERVER, strerror(errno));
@@ -163,37 +165,60 @@ gfw_resolve(const char *hostname, char *out_ip)
 
 
 	/* 构造DNS数据包 */
-	dns_data = build_request_data(hostname, &data_size);
+	/* 将hostname复制到host_buf，因为调用函数里的strtok会修改hostname */
+	host_buf = (char *) calloc(1, strlen(hostname) + 1);
+	memcpy(host_buf, hostname, strlen(hostname));
+	dns_data = build_request_data(host_buf, &data_size);
 
+
+	printf("send dns packet:\n");
 	char *pbuf = dns_data;
 	for (i = 0; i < data_size; ++i)
 		printf("%02X ", (unsigned char) pbuf[i]);
 	printf("\n");
 
 
-	/* 发送 */
+	/* 发送DNS数据包 */
 	n = sendto(sockfd, dns_data, data_size, 0,
-		(struct sockaddr *) &dest, addr_len);
+			(struct sockaddr *) &dest, addr_len);
 	if (n == -1) {
 		perror("sendto() failed");
 		exit(errno);
 	}
 
 
-
+	/* 接收返回的DNS响应包 */
 	for (i = 0; i < MAX_WAIT_TIMES; ++i) {
 		memset(recv_buf, 0, BUFSIZE);
 
 		n = recvfrom(sockfd, recv_buf, BUFSIZE, 0,
 			(struct sockaddr *) &dest, &addr_len);
+		if (n == -1) {
+			perror("recvfrom() failed");
+			exit(errno);
+		}
 
 		printf("recv %d bytes:\n", n);
-		int j = 0;
+		int j;
 		for (j = 0; j < n; ++j)
 			printf("%02X ", (unsigned char) recv_buf[j]);
 		printf("\n");
+
+		/* 解析返回的响应包 */
+		decode_dns_response(recv_buf, hostname, out_ip);
+		break;
+
+
+		/* 再次检测IP是否在黑名单上，如果不在则跳出 */
+		//if (is_bad_ip(out_ip) == 0)
+			//break;
+		/* 如果IP仍在黑名单上，继续等待接收DNS返回的数据，总共尝试
+		 * MAX_WAIT_TIMES 次 */
 	}
 
+	/* 记得释放 */
+	free(dns_data);
+	free(host_buf);	/* 这里释放的host_buf会不会有问题，毕竟strtok修改过 */
 }
 
 
@@ -228,7 +253,8 @@ get_hostname(const char *hostname, char *out_ip)
 
 	/* address-to-name translation */
 	/* 以一个套接口地址为参数，返回一个描述主机的字符串和
-	 * 一个描述服务的字符串 */
+	 * 一个描述服务的字符串
+	 * 为什么有些地址拿不到IP地址呢 */
 	n = getnameinfo((struct sockaddr *) &sa, sizeof(sa),
 		out_ip, NI_MAXHOST, NULL, 0, 0);
 	if (n != 0) {
@@ -268,7 +294,7 @@ is_bad_ip(char *ip)
  * 构造DNS查询数据包
  */
 char *
-build_request_data(const char *hostname, int *size)
+build_request_data(char *hostname, int *size)
 {
 	struct dnshdr dns_header;
 	/* size = header + (1 + hostname + 1) + type + class */
@@ -299,7 +325,9 @@ build_request_data(const char *hostname, int *size)
 	/* 查询名的格式为: 长度(1字节)+N字节内容(N由前面的长度定义)+...+长度0 */
 	/* 即比如www.github.com，变为 3www6github3com0 */
 	/* 提取hostname中的'.' */
-	pstr = strtok((char *) hostname, ".");
+	/* !!! 注意：strtok()会修改hostname的!!!
+	 * 因此在调用函数前，我们把hostname复制到host_buf */
+	pstr = strtok(hostname, ".");
 	while (pstr != NULL) {
 		len = (unsigned char) strlen(pstr);
 		pbuf[0] = len;
@@ -333,4 +361,69 @@ build_request_data(const char *hostname, int *size)
 }
 
 
+
+/*
+ * 根据DNS的响应报文得到域名对应的IP地址
+ */
+void
+decode_dns_response(char *buf, const char *hostname, char *ip)
+{
+	struct dnshdr *dns_header = (struct dnshdr *) buf;
+	short ans_num = ntohs(dns_header->answers);	/* 回答数量 */
+	unsigned char *pbuf;
+	int i;
+	unsigned short query_type, data_len;
+
+	printf("num = %d\n", ans_num);
+
+
+	/* 直接跳到 回答区域 */
+	/* Answers段有 Name, Type, Class, Time to live, Data length 和Address */
+	/* 我们要拿出Address的值从而计算出对应的IP地址 */
+	int len = (sizeof(struct dnshdr) + (1 + strlen(hostname) + 1) + 4);
+	pbuf = (unsigned char *) (buf + len);
+
+
+	for (i = 0; i < ans_num; ++i) {
+		/* 回答区域一般开头都是 C0 0C
+		 * C0：这时不是表示接下来的内容有多长，而是接下来的内容在
+		 * 偏移量中，0C:十进制是12的意思，就是偏移12个字节。
+		 * 从头开始12位，因为Header结构是固定的12字节，所以偏移0C
+		 * 就是到了Question的Name字段 */
+		if (pbuf[0] == 0xC0 && pbuf[1] == 0x0C)
+			pbuf += 2;
+		else
+			pbuf += (1 + strlen(hostname) + 1);
+
+
+
+		memcpy(&query_type, pbuf, sizeof(query_type));
+		query_type = ntohs(query_type);
+		//printf("type = 0x%04X\n", query_type);
+
+		pbuf = pbuf + 2 + 2 + 4;
+		memcpy(&data_len, pbuf, sizeof(data_len));
+		data_len = ntohs(data_len);
+		//printf("len = 0x%04X\n", data_len);
+
+
+		pbuf += 2;
+		/* Type 为 A记录 */
+		/* 如果有多个A记录，则需要保存对应的IP地址 */
+		if (query_type == 0x0001 && data_len == 0x0004) {
+			struct sockaddr_in addr;
+			memcpy(&addr.sin_addr, pbuf, sizeof(addr.sin_addr));
+			if (inet_ntop(AF_INET, &(addr.sin_addr),
+				ip, NI_MAXHOST) == NULL)
+			{
+				perror("inet_pton() failed");
+			} else {
+				printf("ip = %s\n", ip);
+			}
+		}
+
+		/* 下一个记录 */
+		pbuf += data_len;
+	}
+}
 
