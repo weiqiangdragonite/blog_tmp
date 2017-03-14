64cfd1c5f1788149f55ae3e17ba7f9444fc4a14e
weiqiangdragonite <weiqiangdragonite@gmail.com>
2017-03-14 20:02:07 +0800

    update

--- python/爬虫/crawl_ip/crawl_ip.py
+++ python/爬虫/crawl_ip/crawl_ip.py
@@ -12,6 +12,7 @@ headers = { \
 }
 
 
+# 通过ip138
 def query_ip(ip):
     info = {}
     url = "http://www.ip138.com/ips138.asp?ip={ip}".format(ip=ip)
@@ -21,10 +22,28 @@ def query_ip(ip):
 
     if len(data):
         data = str(data)
-        info[ip] = data[data.find("：")+1:data.find("</li>")]
+        s = data[data.find("：")+1:data.find("</li>")]
+        info[ip] = ' '.join(s.split())
     else:
         info[ip] = ''
     print(info)
+    return info
+
+def query_ip_2(ip):
+    info = {}
+    url = "http://www.baidu.com/s?wd={ip}".format(ip=ip)
+    res = requests.get(url, timeout = 60, headers = headers)
+    page = pq(res.content)
+    data = page("span.c-gap-right")
+
+    if len(data):
+        data = str(data)
+        s = data[data.find('</span>')+7:]
+        info[ip] = ' '.join(s.split())
+    else:
+        info[ip] = ''
+    print(info)
+    return info
 
 
 if __name__ == "__main__":
